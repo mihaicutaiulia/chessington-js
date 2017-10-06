ba007d74d35f1c97b19ec0227c1177fb262b7da5
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:49:17 +0100

    Add tests that certain pieces cannot leave the board

--- tests/engine/pieces/pawn.spec.ts
+++ tests/engine/pieces/pawn.spec.ts
@@ -41,6 +41,15 @@ describe('Pawn', () => {
             moves.should.deep.include.members([Square.at(2, 7), Square.at(3, 7)]);
         });
 
+        it('cannot move at the top of the board', () => {
+            const pawn = new Pawn(Player.WHITE);
+            board.setPiece(Square.at(7, 3), pawn);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.be.empty;
+        });
+
     });
 
     describe('black pawns', () => {
@@ -77,6 +86,14 @@ describe('Pawn', () => {
             moves.should.deep.include.members([Square.at(4, 7), Square.at(5, 7)]);
         });
 
+        it('cannot move at the bottom of the board', () => {
+            const pawn = new Pawn(Player.BLACK);
+            board.setPiece(Square.at(0, 3), pawn);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.be.empty;
+        });
     });
 
     describe('pawns interaction', () => {
