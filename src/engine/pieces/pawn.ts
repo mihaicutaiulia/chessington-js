9a3eae634d204e53e92d18f2f3c299df82c4ff11
Iulia Mihaicuta <Iulia.Mihaicuta@softwire.com>
2025-07-07 12:45:19 +0300

    pawn can move one square up/down

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,31 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves;
+        const currentPosition : Square = board.findPiece(this);
+
+        if (this.player == Player.WHITE) {
+            moves = this.getAvailableMovesWhite(currentPosition);
+        } else {
+            moves = this.getAvailableMovesBlack(currentPosition)
+        }
+
+        return moves;
+    }
+
+    private getAvailableMovesWhite(currentPosition: Square) : Array<Square> {
+        const moves = [];
+
+        moves.push(Square.at(currentPosition.row + 1, currentPosition.col));
+
+        return moves;
+    }
+
+    private getAvailableMovesBlack(currentPosition: Square) : Array<Square> {
+        const moves = [];
+
+        moves.push(Square.at(currentPosition.row - 1, currentPosition.col));
+
+        return moves;
     }
 }
