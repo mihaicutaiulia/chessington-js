1913e8fcf6891202614de8b9976c3a8b812c8819
Iulia Mihaicuta <Iulia.Mihaicuta@softwire.com>
2025-07-07 15:01:27 +0300

    add implementation for bishop movement

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,21 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const moves = [];
+        const currentPosition : Square = board.findPiece(this);
+
+        for (let i = 0; i < 8; ++i) {
+            for (let j = 0; j < 8; ++j) {
+                const iDiff = i - currentPosition.row;
+                const jDiff = j - currentPosition.col;
+                if (iDiff == jDiff || iDiff == -jDiff) {
+                    if (i != currentPosition.row || j != currentPosition.col) {
+                        moves.push(Square.at(i, j));
+                    }
+                }
+            }
+        }
+
+        return moves;
     }
 }
