117202e5bacca161ca3bfc1fa8c215f0d9701d01
Iulia Mihaicuta <Iulia.Mihaicuta@softwire.com>
2025-07-07 15:41:01 +0300

    add implementation for king movement

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,21 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const moves = [];
+        const currentPosition : Square = board.findPiece(this);
+
+        const x = currentPosition.row;
+        const y = currentPosition.col;
+
+        for (let i = -1; i < 2; ++i) {
+            for (let j = -1; j < 2; ++j) {
+                if (this.isPositionValid(x + i, y + i) && (i != 0 || j != 0)) {
+                    moves.push(Square.at(x + i, y + j));
+                }
+            }
+        }
+
+        return moves;
+
     }
 }
